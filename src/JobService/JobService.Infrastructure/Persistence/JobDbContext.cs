189837ebb112a6cfaa2dcd37941b2f8f7ac0a428
AyushDDas <ayush100das@gmail.com>
2026-05-23 21:44:17 +0530

    feat: add ef core infrastructure and repository implementation

---
+++ src/JobService/JobService.Infrastructure/Persistence/JobDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JobService.Infrastructure.Persistence
+{
+    internal class JobDbContext
+    {
+    }
+}
---
+++ src/JobService/JobService.Infrastructure/Repositories/JobPostRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JobService.Infrastructure.Repositories
+{
+    internal class JobPostRepository
+    {
+    }
+}
